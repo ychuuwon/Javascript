12ec6de53297abbd80db50e83dc5d66a260d69ca
Joédio Borges Jr <joedio.borges@ifc.edu.br>
2024-12-06 09:02:18 -0300

    Teste de escopo

--- variaveis.js
+++ variaveis.js
@@ -1,15 +1,22 @@
+var nome = "Joédio";
+console.log(nome);
+
 console.log("---- var ----");
 if (true) {
+    console.log("Acessível dentro do bloco: " + nome);
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
 
 console.log("---- let ----");
+let sobrenome = "Borges";
 if (true) {
+    console.log(sobrenome); //  funciona aqui.
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
+//console.log(exemploLet); // Erro: exemploLet não está definido.
+console.log("Acessível globalmente: " + sobrenome);
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
