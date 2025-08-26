ecaecd1553bc82f797a99208e2899cbdacc0d0a1
Júlia Guterres Pellin <juuhp2008@gmail.com>
2025-08-26 09:29:50 -0300

    versão calculadora

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -1,4 +1,4 @@
 const saudacao = require('./meuModulo'); // Importando o módulo
 
-const mensagem = saudacao('Joédio'); // Executando a função
+const mensagem = saudacao('Juia Sigma'); // Executando a função
 console.log(mensagem);
---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,14 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+// Exemplo de uso:
+const soma = funcoesMatematicas.somar(5, 3);
+console.log(`Soma: ${soma}`);  
+
+const subtracao = funcoesMatematicas.subtrair(10, 4);
+console.log(`Subtração: ${subtracao}`);
+
+const multiplicacao = funcoesMatematicas.multiplicar(7, 7);
+console.log(`Multiplicação: ${multiplicacao}`);
+
+const divisao = funcoesMatematicas.dividir(7, 7);
+console.log(`Divisão: ${divisao}`);
